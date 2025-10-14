a8aee5be7b1655514214ef590520ca83013e8e78
Eduardoedy17 <eduardoedy1717@gmail.com>
2025-10-13 23:53:45 -0300

    ...

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 # Criação da aplicação Flask.
 app = Flask(__name__)
 
+# Desabilitar o cache do Jinja2 para desenvolvimento.
+app.jinja_env.cache = {}
+
 # Rotas da aplicação e navegação entre páginas.
 @app.route('/')
 def home():
@@ -15,6 +18,20 @@ def sobre_sistema():
 @app.route('/ajuda')
 def ajuda_sistema():
     return render_template('ajuda.html')
+
+@app.route('/contato')
+def contato_sistema():
+    return render_template('contato.html')
+
+@app.route('/aluno')
+def aluno_listar():
+    return render_template('aluno/lista_A.html')
+
+@app.route('/professor')
+def professor_listar():
+    return render_template('professor/lista_P.html')
+
+
 
 
 
