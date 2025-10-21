8a167f8c457199f20f8b044f8b98a85bdaea0f8a
Eduardoedy17 <eduardoedy1717@gmail.com>
2025-10-21 19:18:23 -0300

    Ajuste Join turmas

--- dao/turma_dao.py
+++ dao/turma_dao.py
@@ -2,7 +2,9 @@ from dao.db_config import get_db_connection
 
 class TurmaDAO:
 
-    sqlSelect = "SELECT id, semestre, curso_id, professor_id FROM turma"
+    sqlSelect = """ SELECT t.id, semestre, nome_curso,  p.nome FROM turma t
+                join curso c on c.id = t.curso_id
+                join professor p on p.id  = t.professor_id """
 
     def listar(self):
         conn = get_db_connection()
