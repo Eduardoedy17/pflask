540f5482177de10ebbcb284805a5502e66fe84a0
Eduardoedy17 <eduardoedy1717@gmail.com>
2025-11-09 18:37:04 -0300

    Enviando parametros

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dao.aluno_dao import AlunoDAO
 from dao.professor_dao import ProfessorDAO
 from dao.curso_dao import CursoDAO
@@ -53,10 +53,44 @@ def listar_turma():
     lista = dao.listar()
     return render_template('turma/lista.html', lista=lista)
 
+@app.route('/saudacao1/<nome>')
+def saudacao1(nome):
+    # dao.salvar(nome)
+    return render_template('saudacao/saudacao.html', nome_recebido=nome)
 
+@app.route('/saudacao2/')
+def saudacao2():
+    nome = request.args.get('nome')
+    return render_template('saudacao/saudacao.html', nome_recebido=nome)
 
+@app.route('/login', methods=['POST'])
+def login():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    email = request.form['email']
+    dados = f"Usuário: {usuario}, Senha: {senha}, E-mail: {email}"
+    return render_template('saudacao/saudacao.html', nome_recebido=dados)
 
+# Permita GET e POST
+@app.route('/desafio', methods=['GET', 'POST'])
+def desafio():
+    # Crie a variável para os dados, começando como nula
+    dados_recebidos = None 
+    
+    # Se a requisição for POST (formulário enviado)
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        dt_nascimento = request.form['dt_nascimento']
+        cpf = request.form['cpf']
+        nome_mae = request.form['nome_mae']
+        # Preencha a variável com os dados
+        dados_recebidos = f"Nome: {nome}, E-mail: {email}, Data de Nascimento: {dt_nascimento}, CPF: {cpf}, Nome da Mãe: {nome_mae}"
 
+    # Renderize a página em AMBOS os casos (GET ou POST)
+    # Se for GET, dados_recebidos será None
+    # Se for POST, dados_recebidos terá os dados do formulário
+    return render_template('desafio/desafio1.html', nome_recebido=dados_recebidos)
 
 #Método 'main' sempre no final do arquivo.
 if __name__ == '__main__':
